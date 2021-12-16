99f006e813aa8baa1411eb073a57d799916ea6f6
leafywz <2459972548@qq.com>
2021-12-16 23:56:51 +0800

    main

--- 03/assets/js/sketch.js
+++ 03/assets/js/sketch.js
@@ -1,8 +1,47 @@
+///////////////////////////////////////////////////
+//===============================================//
+//Floating Light v1.0 - By Shangjing Lin(Stanley)//
+//===============================================//
+///////////////////////////////////////////////////
+
+/*
+This work was originally inspired by Van Gogh's starry night and scraped painting.
+I have seen the dynamic starry night made by openFramework, it's wonderful and amazing.
+This is the reason why I decided to make a dynamic drawing tool.
+You can control the flow of time and create dynamic particles, 
+so it is easy to complete the amazing effect.
+
+Shortcut key:
+0-9 : Select colors.
+D : Switch between day and night.
+B : Switch the brush type.
+Space : Play/Pause.
+T : Timer tool.
+E : Eraser.
+C : Clear canvas.
+S : Save canvas to PNG file.
+L Shift: Hide the menu.
+*/
+
+// 待修改
+
+////////////////////////////////////////////
+//Global Variable
+////////////////////////////////////////////
+var objs = [];
+var btns = [];
+var FPS = 60;
+var timepast = 0;
+var speed = 5000000;
+var isPlaying = true;
+var isAcceleratoring = false;
+var isMenuHide = false;
+
+// node tree
 var tree
 var data_list = [19, 7, 1, 13, 9, 11, 29, 33]
-var btns = [] //按钮
 var input // 键盘数字输入
-traversal_list = [] //
+traversal_list = []
 
 var node_select = null
 
@@ -13,44 +52,175 @@ var flag_tr = false
 
 var go = 0
 
-function Button(x, y, width, heigth, text) {
-    //位置大小及内容属性
-    this.x = x
-    this.y = y
-    this.width = width
-    this.heigth = heigth
-    this.text = text
 
-    //颜色属性 白色背景
-    this.r = 255
-    this.g = 255
-    this.b = 255
+////////////////////////////////////////////
+//FunctionButton
+////////////////////////////////////////////
+function FuncBtn(X, Y, W, H, CMD) {
+    this.x = X;
+    this.y = Y;
+    this.w = W;
+    this.h = H;
+    this.cmd = CMD;
 }
 
-Button.prototype.displayBtn = function () {
-    fill(this.r, this.g, this.b)
-    rect(this.x, this.y, this.width, this.heigth, 10)
-    textAlign(CENTER)
-    fill(0) //黑色字体
-    text(this.text, this.x + this.width / 2, this.y + this.heigth / 2 + 5)
-}
-
-Button.prototype.clicked = function () {
-    if (mouseIsPressed == true) {
-        if (mouseX >= this.x && mouseX <= this.x + this.width &&
-            mouseY >= this.y && mouseY <= this.y + this.heigth) {
-            this.r = 255
-            this.g = 100
-            this.b = 100
-            return true
-        }
+// 鼠标在按钮上
+FuncBtn.prototype.isMouseInBtn = function () {
+    if (mouseX >= this.x && mouseX <= this.x + this.w &&
+        mouseY >= this.y && mouseY <= this.y + this.h) {
+        return true;
     } else {
-        this.r = 255
-        this.g = 255
-        this.b = 255
-        return false
+        return false;
     }
 }
+
+// 点击后的时间响应
+FuncBtn.prototype.clickBtn = function () {
+    print("ClickBtn!");
+    if (this.cmd == "pause") {
+        isPlaying = false;
+        for (var i = 0; i < objs.length; i++) {
+            objs[i].isPlaying = false;
+        }
+        this.cmd = "play";
+    } else if (this.cmd == "play") {
+        isPlaying = true;
+        for (var i = 0; i < objs.length; i++) {
+            objs[i].isPlaying = true;
+        }
+        this.cmd = "pause";
+    } else if (this.cmd == "accelerator") {
+        isAcceleratoring = true
+        speed = 1000000
+        this.cmd = "decelerate";
+    } else if (this.cmd == "decelerate") {
+        isAcceleratoring = false
+        speed = 5000000
+        this.cmd = "accelerator";
+    } else if (this.cmd == "clear") {
+        tree.Reset(tree.root)
+    } else if (this.cmd == "save") {
+        saveCanvas("Painting", "png")
+    } else if (this.cmd == "search") {
+        if (node_select) {
+            tree.Find(tree.root, node_select.value)
+            flag_sr = true
+        }
+    } else if (this.cmd == "insert") {
+        var in_data = input.value()
+        // 检查输入的数字是否合理
+        if (!tree.isInTree(tree.root, in_data) && in_data != '') {
+            console.log('success')
+            tree.Insert_one(tree.root, int(in_data))
+            flag_in = true
+        } else {
+            console.log('the value is in')
+        }
+
+    } else if (this.cmd == "remove") {
+        if (node_select) {
+            tree.Remove(tree.root, node_select.value)
+            console.log(traversal_list)
+            flag_re = true
+        }
+    } else if (this.cmd == "traversal") {
+        tree.Traversal(tree.root)
+        flag_tr = true
+    }
+}
+
+
+FuncBtn.prototype.displayBtn = function () {
+    stroke(0);
+    strokeWeight(1);
+    fill(255, 255, 255);
+    rect(this.x, this.y, this.w, this.h, 5);
+
+    if (this.cmd == "pause") {
+        fill(0);
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        rectMode(CENTER);
+        rect(-4, 0, 4, 15);
+        rect(4, 0, 4, 15);
+        rectMode(CORNER);
+        resetMatrix();
+    } else if (this.cmd == "play") {
+        fill(0);
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        triangle(-2, -8, -2, 8, 6, 0);
+        resetMatrix();
+    } else if (this.cmd == "accelerator" || this.cmd == "decelerate") {
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        noFill();
+        ellipse(0, 0, 22, 22);
+        ellipse(0, 0, 25, 25);
+        fill(0);
+        ellipse(0, 0, 3, 3);
+        strokeWeight(2);
+        line(0, 0, 5, 0);
+        line(0, 0, 0, -7);
+        resetMatrix();
+    } else if (this.cmd == "clear") {
+        fill(0);
+        noStroke();
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        textSize(15);
+        textAlign(CENTER);
+        textStyle(BOLD);
+        text("Clear", 0, 5);
+        resetMatrix();
+    } else if (this.cmd == "save") {
+        fill(0);
+        noStroke();
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        textSize(15);
+        textAlign(CENTER);
+        textStyle(BOLD);
+        text("Save", 0, 5);
+        resetMatrix();
+    } else if (this.cmd == "search") {
+        fill(0);
+        noStroke();
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        textSize(15);
+        textAlign(CENTER);
+        textStyle(BOLD);
+        text("Search", 0, 5);
+        resetMatrix();
+    } else if (this.cmd == "insert") {
+        fill(0);
+        noStroke();
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        textSize(15);
+        textAlign(CENTER);
+        textStyle(BOLD);
+        text("Insert", 0, 5);
+        resetMatrix();
+    } else if (this.cmd == "remove") {
+        fill(0);
+        noStroke();
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        textSize(15);
+        textAlign(CENTER);
+        textStyle(BOLD);
+        text("remove", 0, 5);
+        resetMatrix();
+    } else if (this.cmd == "traversal") {
+        fill(0);
+        noStroke();
+        translate(this.x + this.w / 2, this.y + this.h / 2);
+        textSize(15);
+        textAlign(CENTER);
+        textStyle(BOLD);
+        text("traversal", 0, 5);
+        resetMatrix();
+    }
+}
+
+
+////////////////////////////////////////////
+//Node 
+////////////////////////////////////////////
 
 
 function Node(value, left, right) {
@@ -66,7 +236,15 @@ function Node(value, left, right) {
     this.R = 255
     this.G = 255
     this.B = 255
-    this.state = 0
+
+    //其他属性
+    this.state = 0 //节点是否处于选中状态
+
+    this.line_l = false //左边线颜色
+    this.line_r = false //右边线颜色
+
+    this.isfirst = true //是否为第一次访问
+    this.border_color = false //边框的颜色是否变化
 }
 
 Node.prototype.ChangeColor = function (r, g, b) {
@@ -76,22 +254,39 @@ Node.prototype.ChangeColor = function (r, g, b) {
 }
 
 Node.prototype.Draw = function () {
-    stroke(255, 0, 0)
-    strokeWeight(3);
+    stroke(0)
+    strokeWeight(2);
     if (this.left != null) {
+        if (this.line_l) {
+            stroke(250, 132, 43)
+        }
         line(this.x, this.y, this.left.x, this.left.y)
     }
+    stroke(0)
     if (this.right != null) {
+        if (this.line_r) {
+            stroke(250, 132, 43)
+        }
         line(this.x, this.y, this.right.x, this.right.y)
     }
     stroke(0)
-    strokeWeight(1);
+    if (this.border_color) {
+        stroke(250, 132, 43)
+    }
+    strokeWeight(2);
     fill(this.R, this.G, this.B)
     ellipse(this.x, this.y, this.r, this.r)
     fill(0)
+    strokeWeight(0);
+    textSize(15)
     textAlign(CENTER)
     text(this.value, this.x, this.y + 5)
 }
+
+
+////////////////////////////////////////////
+//Tree 
+////////////////////////////////////////////
 
 function BinaryTree() {
     this.root = null
@@ -118,6 +313,10 @@ BinaryTree.prototype.Graph = function (root, x, y, width) {
 
 BinaryTree.prototype.Reset = function (root) {
     root.ChangeColor(255, 255, 255)
+    root.line_l = false
+    root.line_r = false
+    root.border_color = false
+    root.isfirst = true
     root.state = 0
     if (root.right != null) {
         arguments.callee(root.right)
@@ -161,6 +360,7 @@ BinaryTree.prototype.Traversal = function (root) {
     if (root.left != null) {
         arguments.callee(root.left)
     }
+    traversal_list.push(root)
     if (root.right != null) {
         arguments.callee(root.right)
     }
@@ -173,7 +373,7 @@ BinaryTree.prototype.Select = function (root) {
     while (stack.length) {
         root = stack.pop() //弹出并访问当前节点
         //检测是否按下
-        if (mouseX >= root.x - root.r && mouseX <= root.x + root.r && 
+        if (mouseX >= root.x - root.r && mouseX <= root.x + root.r &&
             mouseY >= root.y - root.r && mouseY <= root.y + root.r) {
             if (mouseIsPressed == true) {
                 if (root.state == 0) {
@@ -191,11 +391,11 @@ BinaryTree.prototype.Select = function (root) {
                 }
             }
         }
-        if (root.left) {
-            stack.push(root.left)
-        }
         if (root.right) {
             stack.push(root.right)
+        }
+        if (root.left) {
+            stack.push(root.left)
         }
     }
 }
@@ -319,7 +519,6 @@ BinaryTree.prototype.Find_P = function (root, item, seed = null) {
 // 设被删除的节点为P 其父节点为PP
 BinaryTree.prototype.Remove = function (root, item) {
     var P = this.Find(root, item)
-    traversal_list = []
     var PP = this.Find_P(root, item)
     if (P == root) {
         return
@@ -392,39 +591,94 @@ BinaryTree.prototype.Remove = function (root, item) {
     }
 }
 
+////////////////////////////////////////////
+//Setup
+////////////////////////////////////////////
 
 function setup() {
+    frameRate(FPS);
     var myCanvas = createCanvas(1080, 400);
     myCanvas.parent("p5_canvas");
-    frameRate(60) //原数据为3
+    noCursor();
+    strokeCap(PROJECT);
+
+    // Tree
     tree = new BinaryTree()
     tree.root = tree.Insert(data_list)
     tree.Graph(tree.root, width / 2, 60, 1.6)
+
+    //Function Buttons
+    btns.push(new FuncBtn(25, 20, 80, 32, "search"));
+    btns.push(new FuncBtn(25, 20 + 40 * 1, 80, 32, "insert"));
+    btns.push(new FuncBtn(25, 20 + 40 * 2, 80, 32, "remove"));
+    btns.push(new FuncBtn(25, 20 + 40 * 3, 80, 32, "traversal"));
+    if (isPlaying) {
+        btns.push(new FuncBtn(25, 20 + 40 * 4, 80, 32, "pause"));
+    } else {
+        btns.push(new FuncBtn(25, 20 + 40 * 4, 80, 32, "play"));
+    }
+    if (isAcceleratoring) {
+        btns.push(new FuncBtn(25, 20 + 40 * 5, 80, 32, "accelerator"));
+    } else {
+        btns.push(new FuncBtn(25, 20 + 40 * 5, 80, 32, "decelerate"));
+    }
+    btns.push(new FuncBtn(25, 20 + 40 * 6, 80, 32, "clear"));
+    btns.push(new FuncBtn(25, 20 + 40 * 7, 80, 32, "save"));
+
+    //input 
     input = createInput()
-    input.position(220, 195)
-    // tree.Draw(tree.root)
-
-    // 初始化按钮
-    btns.push(new Button(25, 30, 80, 40, 'Search'))
-    btns.push(new Button(25, 30 + 50 * 1, 80, 40, 'Insert'))
-    btns.push(new Button(25, 30 + 50 * 2, 80, 40, 'Remove'))
-    btns.push(new Button(25, 30 + 50 * 3, 80, 40, 'Traversal'))
-    btns.push(new Button(25, 30 + 50 * 4, 80, 40, 'Reset'))
-
+    input.position(220, 180)
 }
 
 
+////////////////////////////////////////////
+//Draw
+////////////////////////////////////////////
 function draw() {
-    background(255, 245, 225)
+    background(255, 245, 225);
+    timepast += 1 / FPS;
+    if (!isMenuHide) {
+        if (timepast < 2) {
+            noStroke();
+            textAlign(LEFT);
+            textSize(15);
+            fill(0);
+            text("Floating Light v1.0 - Made By Shangjing Lin(Stanley)", 10, height - 10);
+        } else if (timepast < 5) {
+            noStroke();
+            textAlign(LEFT);
+            textSize(15);
+            fill(0);
+            text("Press Left Shift to hide Menu, Press S to save canvas to PNG.", 10, height - 10);
+        }
+    }
+
+
+    // 检查当前是否右节点被选中
     tree.Select(tree.root)
+
+    //===================
+    //Drawing Something
+    //===================
+    tree.Draw(tree.root)
+
     // 搜索节点动画
-    if (flag_sr) {
-        for (let i = 0; i < 4000000; i++) {
+    if (flag_sr && isPlaying) {
+        for (let i = 0; i < speed; i++) {
             let date = new Date()
         }
-        // console.log(traversal_list)
         if (go < traversal_list.length) {
-            traversal_list[go++].ChangeColor(255, 0, 0)
+
+            //下一个节点在该节点的左还是右
+            if (traversal_list[go].left == traversal_list[go + 1]) {
+                traversal_list[go].line_l = true
+            }
+            if (traversal_list[go].right == traversal_list[go + 1]) {
+                traversal_list[go].line_r = true
+            }
+            traversal_list[go].border_color = true //边框亮起
+            traversal_list[go++].ChangeColor(250, 132, 43)
+
         } else {
             tree.Reset(tree.root)
             node_select.state = 0
@@ -435,67 +689,62 @@ function draw() {
         }
     }
 
-    // 搜索节点
-    if (btns[0].clicked() && node_select) {
-        tree.Find(tree.root, node_select.value)
-        console.log(traversal_list)
-        flag_sr = true
-    }
-
     //插入节点动画
-    if (flag_in) {
-        for (let i = 0; i < 3000000; i++) {
+    if (flag_in && isPlaying) {
+        for (let i = 0; i < speed; i++) {
             let date = new Date()
         }
-        tree.Graph(tree.root, width / 2, 60, 1.6)
-        traversal_list = []
-        flag_in = false
-    }
-
-    //插入节点
-    if (btns[1].clicked()) {
-        var in_data = input.value()
-        // 检查输入的数字是否合理
-        if (!tree.isInTree(tree.root, in_data) && in_data != '') {
-            console.log('success')
-            tree.Insert_one(tree.root, int(in_data))
-            flag_in = true
-        } else {
-            console.log('the value is in')
+        if (go < 3) {
+            go += 2
+        } else{
+            tree.Graph(tree.root, width / 2, 60, 1.6)
+            traversal_list = []
+            flag_in = false
         }
-
     }
 
     //删除节点动画
-    if (flag_re) {
-        for (let i = 0; i < 7000000; i++) {
-            let date = new Date()
-        }
-        tree.Graph(tree.root, width / 2, 60, 1.6)
-        node_select.state = 0
-        node_select = null
-        flag_re = false
-    }
-
-    // 删除节点
-    if (btns[2].clicked() && node_select) {
-        tree.Remove(tree.root, node_select.value)
-        if (traversal_list.length == 2) {
-            beginShape()
-            curveVertex(traversal_list[0].x, traversal_list[0].y)
-            curveVertex(traversal_list[1].x, traversal_list[1].y)
-            endShape()
-        }
-        flag_re = true
-    }
-
-    //先序遍历动画
-    if (flag_tr) {
-        for (let i = 0; i < 3000000; i++) {
+    if (flag_re && isPlaying) {
+        for (let i = 0; i < speed; i++) {
             let date = new Date()
         }
         if (go < traversal_list.length) {
-            traversal_list[go++].ChangeColor(255, 255, 0)
+            go += 2
+        } else {
+            tree.Graph(tree.root, width / 2, 60, 1.6)
+            tree.Reset(tree.root)
+            node_select.state = 0
+            node_select = null
+            traversal_list = []
+            go = 0
+            flag_re = false
+        }
+
+    }
+
+    //先序遍历动画
+    if (flag_tr && isPlaying) {
+        for (let i = 0; i < speed; i++) {
+            let date = new Date()
+        }
+        if (go < traversal_list.length) {
+
+            //下一个节点在该节点的左还是右
+            if (traversal_list[go].left == traversal_list[go + 1]) {
+                traversal_list[go].line_l = true
+            }
+            if (traversal_list[go].right == traversal_list[go + 1]) {
+                traversal_list[go].line_r = true
+            }
+
+            // 是否第一次访问该节点
+            if (traversal_list[go].isfirst) { //是否第一次访问该节点
+                traversal_list[go].border_color = true //边框亮起
+                traversal_list[go++].isfirst = false
+            } else {
+                traversal_list[go++].ChangeColor(250, 132, 43)
+            }
+
         } else {
             tree.Reset(tree.root)
             traversal_list = []
@@ -504,23 +753,65 @@ function draw() {
         }
     }
 
-    // 先序遍历
-    if (btns[3].clicked()) {
-        tree.Traversal(tree.root)
-        flag_tr = true
+    //=======================
+    //Cursor Icon
+    //=======================
+    //Menu
+    stroke(0);
+    strokeWeight(2);
+    if (!isMenuHide) {
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].displayBtn();
+            if (btns[i].isMouseInBtn()) {
+                cursor(HAND);
+            }
+        }
     }
 
-    // 重置
-    if (btns[4].clicked()) {
-        tree.Reset(tree.root)
-        traversal_list = []
-        go = 0
+    //Canvas
+    if (mouseX > 105 || isMenuHide) {
+        cursor(ARROW);
     }
 
-    // 更新树
-    tree.Draw(tree.root)
-    //更新按钮
-    for (let i = 0; i < btns.length; i++) {
-        btns[i].displayBtn()
+}
+
+function mouseClicked() {
+    if (!isMenuHide) {
+        for (var i = 0; i < btns.length; i++) {
+            if (btns[i].isMouseInBtn()) {
+                btns[i].clickBtn();
+            }
+        }
+    }
+    return false;
+}
+
+////////////////////////////////////////////
+//Shortcut Key
+////////////////////////////////////////////
+
+function keyPressed() {
+    //print("keyCode is"+keyCode);
+    if (keyCode == 83) { //S
+        btns[0].clickBtn();
+    }
+    if (keyCode == 73) { //I
+        btns[1].clickBtn();
+    }
+    if (keyCode == 82) { //R
+        btns[2].clickBtn();
+    }
+    if (keyCode == 84) { //T
+        btns[3].clickBtn();
+    }
+    if (keyCode == 32) { //Space  暂停键
+        btns[4].clickBtn();
+    }
+    if (keyCode == 16) { //Shift L 工具栏
+        isMenuHide = !isMenuHide;
     }
 }
+
+////////////////////////////////////////////
+//To be continued...
+////////////////////////////////////////////
